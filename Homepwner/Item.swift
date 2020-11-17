7f18bfec5ace08394672b6b0b72762e5ca83c0a6
Nick Hella <NickHella@macxbook.local>
2020-11-16 19:41:40 -0500

    Initial project based off book exercise

---
+++ Homepwner/Item.swift
@@ -0,0 +1,69 @@
+//
+//  Item.swift
+//  Homepwner
+//
+//  Created by Nick Hella on 10/24/20.
+//
+
+import UIKit
+
+class Item: NSObject, NSCoding {
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
+        aCoder.encode(dateCreated, forKey: "dateCreated")
+        aCoder.encode(itemKey, forKey: "itemKey")
+        aCoder.encode(serialNumber, forKey: "serialNumber")
+        aCoder.encode(valueInDollars, forKey: "valueInDollars")
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        name = aDecoder.decodeObject(forKey: "name") as! String
+        dateCreated = aDecoder.decodeObject(forKey: "dateCreated") as! Date
+        itemKey = aDecoder.decodeObject(forKey: "itemKey") as! String
+        serialNumber = aDecoder.decodeObject(forKey: "serialNumber") as! String?
+        valueInDollars = aDecoder.decodeInteger(forKey: "valueInDollars")
+        super.init()
+    }
+    
+    var name: String
+    var valueInDollars: Int
+    var serialNumber: String?
+    var dateCreated: Date
+    let itemKey: String
+    
+    // Designated initilializer
+    init(name: String, serialNumber: String?, valueInDollars: Int) {
+        self.name = name
+        self.valueInDollars = valueInDollars
+        self.serialNumber = serialNumber
+        self.dateCreated = Date()
+        self.itemKey = UUID().uuidString
+        
+        super.init()
+    }
+    
+    // Convenience initializer 
+    convenience init(random: Bool = false) {
+        if random {
+            let adjectives = ["Fluffy", "Rusty", "Shiny"]
+            let nouns = ["Bear", "Spork", "Mac"]
+            
+            var idx = arc4random_uniform(UInt32(adjectives.count))
+            let randomAdjective = adjectives[Int(idx)]
+            
+            idx = arc4random_uniform(UInt32(nouns.count))
+            let randomNoun = nouns[Int(idx)]
+            
+            let randomName = "\(randomAdjective) \(randomNoun)"
+            let randomValue = Int(arc4random_uniform(100))
+            let randomSerialNumber = UUID().uuidString.components(separatedBy: "-").first!
+            
+            self.init(name: randomName,
+                      serialNumber: randomSerialNumber,
+                      valueInDollars: randomValue)
+        } else {
+            self.init(name: "", serialNumber: nil, valueInDollars: 0)
+        }
+    } // end convenience init
+}
